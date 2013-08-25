8880c150783b6556fb7bab9c6154a97d29762f3d
Alex Kouts <akouts@gmail.com>
2013-08-25 00:33:09 -0700

    Connect Amazon S3

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Omrails::Application.routes.draw do
 
   get 'about' => 'pages#about'
 
+  get 'marketing' => 'pages#marketing'
+
   root :to => 'pins#index'
 
   # The priority is based upon order of creation:
